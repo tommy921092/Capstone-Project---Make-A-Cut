6ae90d0be68dd476cf9b4c135bef1166a0323c2b
Oliver Leung <oliver.hon.pan.leung@gmail.com>
2019-01-22 11:35:58 +0800

    SearchResult with mockAPI

--- frontend/src/page/SearchResults/SearchResults.js
+++ frontend/src/page/SearchResults/SearchResults.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
-import { Segment, Container, Grid } from 'semantic-ui-react'
+import { Segment, Container, Grid, Image, Item, Label } from 'semantic-ui-react'
+import axios from 'axios';
 
 import Footer from './Footer';
 
-import ItemCard from './ItemCard';
-import DisplayList from './DisplayList';
+// import ItemCard from './ItemCard';
+// import DisplayList from './DisplayList';
 import Map from './MapResult';
 
 import './SearchResults.css';
 
+const paragraph = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
 
 export default class SearchResults extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [
-
-        { id: 1, item: <ItemCard shop="Handsome" street="123 Bukkake Road" comment="Straight razor"></ItemCard> },
-        { id: 2, item: <ItemCard shop="Average" street="123 Man Fuk Road" comment="Great deal"></ItemCard> },
-        { id: 3, item: <ItemCard shop="Ugly" street="123 Fuk Yu Road" comment="Quick cut"></ItemCard> },
-        { id: 4, item: <ItemCard shop="Stunning" street="123 Yur mUm" comment="Quick shave"></ItemCard> },
-        { id: 5, item: <ItemCard shop="Stunning" street="123 Yur mUm" comment="Quick shave"></ItemCard> }
-
-      ]
+      list: []
     }
   }
 
-  handleKeyPress = (event) => {
-    if (event.key == 'Enter') {
-      alert('enter press here! ')
-    }
+  componentDidMount() {
+    axios.get(`https://5c4548513858aa001418c3e2.mockapi.io/api/shops/`)
+      .then(res => {
+        console.log(res.data);
+        const list = res.data;
+
+        // const id = list.id
+        // const shop = list.name;
+        // const street = list.street;
+        // const comment = list.tag;
+
+        this.setState(
+          { list }
+        )
+      })
   }
 
   render() {
@@ -39,8 +44,26 @@ export default class SearchResults extends Component {
           <Segment placeholder style={{ opacity: 0.8, padding: '2rem' }}>
             <Grid columns={2}>
 
-              <Grid.Column width={6} style={{overflow: 'auto', maxHeight: '80vh' }}>
-                <DisplayList list={this.state.list}></DisplayList>
+              <Grid.Column width={6} style={{ overflow: 'auto', maxHeight: '80vh' }}>
+                <Item.Group link divided>
+
+                  {this.state.list.map(l =>
+                    <Item>
+                      <Item.Image size='small' rounded src={l.avatar} />
+                      <Item.Content>
+                        <Item.Header as='a'>{l.name}</Item.Header>
+                        <Item.Meta>
+                          <span>{l.street}</span>
+                        </Item.Meta>
+                        <Item.Description>{paragraph}</Item.Description>
+                        <Item.Extra>
+                          <Label>{l.tag}</Label>
+                        </Item.Extra>
+                      </Item.Content>
+                    </Item>
+                    
+                  )}
+                </Item.Group>
               </Grid.Column>
 
               <Grid.Column width={10} verticalAlign='middle' >
