3704e3444b620ccac328151b53ff2663242f7b89
Tommy Tse <tommy921092@msn.com>
2019-01-27 20:21:29 +0800

    merge

---
+++ backend/routes/billing.js
@@ -0,0 +1,18 @@
+const keys = require("../.env");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
+module.exports = app => {
+  app.post("/api/stripe", async (req, res) => {
+    //function for actually charge the user
+    const charge = await stripe.charges.create({
+      amount: 500, // 500 cents = 5usd
+      currency: "usd",
+      source: req.body.id
+    });
+    // for mongo DB
+    //   req.user.credits += 5;
+    //   const user = await req.user.save();
+    // do something for knex DB
+    res.send(user);
+  });
+};
